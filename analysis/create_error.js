f97847b6687d7d4d5bb0dfe4e2d26905af382cae
Will Adair <willhadair@gmail.com>
2024-12-06 19:07:28 -0600

    move around files & add compatibility for jalangi in app.html

---
+++ analysis/create_error.js
@@ -0,0 +1,14 @@
+/*
+The purpose of this file is to introduce errors in the code that the other analyses will catch.
+
+The following errors is introduced in this file:
+1. Introduce a NaN value in the code.
+2. Introduce a function call with more arguments than expected.
+3. Concatenate a string with undefined
+4. Compare a function with a primitive value.
+5. Access an undefined value in an array.
+*/
+
+// do not remove the following comment
+// JALANGI DO NOT INSTRUMENT
+
